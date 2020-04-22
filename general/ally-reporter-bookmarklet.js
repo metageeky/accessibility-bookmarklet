e86b472b8dcb9fe9bebf5a933482e2ae8383e201
kate deibel <kndeibel@syr.edu>
2020-04-22 18:59:53 -0400

    loading notice added

--- general/ally-reporter-bookmarklet.js
+++ general/ally-reporter-bookmarklet.js
@@ -43,19 +43,85 @@ javascript: (function() {
 		}
 	})();
 
+	/* REPORT CODE */
+
+	function loadingNotice() {
+		var notice_style = 'display: flex;flex-direction: row;flex-wrap: nowrap;justify-content: center;align-content: center;align-items: center;position: fixed;height: 100%;width: 100%;background: rgba(0,0,0,0.5);z-index: 9999;top: 0;bottom: 0;left: 0;right: 0;';
+		var notice_msg_style = 'background: #fff;text-align: center;padding: 2em;font-size: 2rem;';
+
+		var notice = document.createElement('DIV');
+		notice.id = 'a11y-loader';
+		notice.style.cssText = notice_style;
+		var notice_msg = document.createElement('DIV');
+		notice_msg.style.cssText = notice_msg_style;
+		notice_msg.innerHTML = 'A11Y Reporter is running...<br>Refresh page to cancel'
+		notice.appendChild(notice_msg);
+		document.body.appendChild(notice);		
+	}
+	
+	function removeLoadingNotice() {
+		document.body.removeChild(document.getElementById('a11y-loader'));
+	}
+	
+	function prepareIframe() {
+		var containerId = "a11y-bookmarklet";
+		var containerStyle = "position: fixed; top: 0; right: 0; max-height: 100%; box-shadow: 0 0 80px rgba(0,0,0,0.3); width: 20%; min-width: 320px; max-width: 450px; z-index: 1000001;";
+
+		var container = document.getElementById(containerId);
+		if(container)
+			document.body.removeChild(container);
+		container = document.createElement("DIV");
+		container.id = containerId;
+		container.style.cssText = containerStyle;
+
+		var iframe = document.createElement("IFRAME");
+		iframe.id = "a11y-iframe";
+		iframe.style.width = "100%", iframe.style.height = "100%", iframe
+			.style.borderWidth = "0";
+
+		var doc;
+
+		container.appendChild(iframe), iframe.onload = function() {
+			function e(e, t) {
+				var i = doc.getElementById(e),
+					o = doc.querySelector(".result");
+				if(i) {
+					var n = function(e) {
+						i.checked ? o.classList.add(t) : o.classList
+							.remove(t), e && updateHeight()
+					};
+					i.addEventListener("change", n, !1), i
+						.addEventListener("click", n, !1), n()
+				}
+			}
+			iframe.onload = function() {}, (doc = iframe.contentWindow
+				.document).open(), doc.write(
+				'<html> <head> <meta name="viewport" content="width=device-width,minimum-scale=1.0,initial-scale=1,user-scalable=yes"> <style> * { margin: 0; padding: 0; border: 0; box-sizing: border-box; } body { font: 14px/1.6 sans; color: #284900; background: rgba(255,255,255,0.95); white-space: nowrap; overflow-x: hidden; text-overflow: ellipsis; padding: 10px; padding-bottom: 30px; } header { display: flex; flex-wrap: nowrap; align-content: baseline; margin-bottom: 10px; } h1 { font-size: 1.2rem; flex: 1 1 auto; } legend { margin-right: 1em; font-weight: bold; } .button-close { padding: 5px 10px; border-radius: 5px; border: 0; font-size: inherit; color: white; background-color: #284900; cursor: pointer; } .button-close:hover { color: white; background-color: #284900; } textarea { border: solid 1px #284900; width: 100%; padding: 5px; resize: none; } </style> </head> <body> <header> <h1>A11Y Report</h1> <button class="button-close" data-action="close">Close</button> </header> <main id="result" class="result">  <textarea id="output" rows="10" wrap="off" spellcheck="false"></textarea> </main> </body> </html> '
+				), doc.close();
+			var t = doc.querySelector('[data-action="close"]');
+			t && t.addEventListener("click", function(e) {
+				document.body.removeChild(container);
+			});
+		}, document.body.appendChild(container);
+
+		return iframe;
+	}
 
 	function runReport() {
+		loadingNotice();
 		var iframe = prepareIframe();
 		iframe.style.display = 'none';
 		Promise.all([
-			loadResource.js('https://metageeky.github.io/accessibility-bookmarklet/general/ally-reporter-support-code.js')
+			loadResource.js('https://metageeky.github.io/accessibility-bookmarklet/general/ally-reporter-support-code.js'),
 			loadResource.js('https://metageeky.github.io/accessibility-bookmarklet/src/axe.min.js'),
 			loadResource.js('https://metageeky.github.io/accessibility-bookmarklet/src/tabbable.js'),
 			loadResource.js('https://metageeky.github.io/accessibility-bookmarklet/src/w3c-alternative-text-computation.js'),
 		]).then(function() {
 			axe.run().then( results => {writeReport(iframe,results)});
+			removeLoadingNotice();			
 		}).catch(function(url) {
 			alert('Resource failed to load:\n'+url);
+			removeLoadingNotice()
 		});
 	}
 
