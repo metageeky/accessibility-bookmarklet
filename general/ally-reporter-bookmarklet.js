b517e4a6927490ccdf0bf5723d1c2dadaceb00ea
kate deibel <the.kate.deibel@gmail.com>
2020-04-23 08:44:34 -0400

    min/max feature

--- general/ally-reporter-bookmarklet.js
+++ general/ally-reporter-bookmarklet.js
@@ -46,7 +46,7 @@ javascript: (function() {
 	/* REPORT CODE */
 
 	function loadingNotice() {
-		var notice_style = 'display: flex;flex-direction: row;flex-wrap: nowrap;justify-content: center;align-content: center;align-items: center;position: fixed;height: 100%;width: 100%;background: rgba(0,0,0,0.5);z-index: 9999;top: 0;bottom: 0;left: 0;right: 0;';
+		var notice_style = 'display: flex;flex-direction: row;flex-wrap: nowrap;justify-content: center;align-content: center;align-items: center;position: fixed;height: 100%;width: 100%;background: rgba(0,0,0,0.5);z-index: 9999;top: 0; bottom: 0;left: 0;right: 0;';
 		var notice_msg_style = 'background: #fff;text-align: center;padding: 2em;font-size: 2rem;';
 
 		var notice = document.createElement('DIV');
@@ -65,7 +65,7 @@ javascript: (function() {
 	
 	function prepareIframe() {
 		var containerId = "a11y-bookmarklet";
-		var containerStyle = "position: fixed; top: 0; right: 0; max-height: 100%; box-shadow: 0 0 80px rgba(0,0,0,0.3); width: 20%; min-width: 320px; max-width: 450px; z-index: 1000001;";
+		var containerStyle = "position: fixed; top: 10px; right: 10px; max-height: 100%; box-shadow: 0 0 80px rgba(0,0,0,0.3); min-width: 320px; max-width: 450px; z-index: 1000001;";
 
 		var container = document.getElementById(containerId);
 		if(container)
@@ -96,12 +96,31 @@ javascript: (function() {
 			}
 			iframe.onload = function() {}, (doc = iframe.contentWindow
 				.document).open(), doc.write(
-				'<html> <head> <meta name="viewport" content="width=device-width,minimum-scale=1.0,initial-scale=1,user-scalable=yes"> <style> * { margin: 0; padding: 0; border: 0; box-sizing: border-box; } body { font: 14px/1.6 sans; color: #284900; background: rgba(255,255,255,0.95); white-space: nowrap; overflow-x: hidden; text-overflow: ellipsis; padding: 10px; padding-bottom: 30px; } header { display: flex; flex-wrap: nowrap; align-content: baseline; margin-bottom: 10px; } h1 { font-size: 1.2rem; flex: 1 1 auto; } legend { margin-right: 1em; font-weight: bold; } .button-close { padding: 5px 10px; border-radius: 5px; border: 0; font-size: inherit; color: white; background-color: #284900; cursor: pointer; } .button-close:hover { color: white; background-color: #284900; } textarea { border: solid 1px #284900; width: 100%; padding: 5px; resize: none; } </style> </head> <body> <header> <h1>A11Y Report</h1> <button class="button-close" data-action="close">Close</button> </header> <main id="result" class="result">  <textarea id="output" rows="10" wrap="off" spellcheck="false"></textarea> </main> </body> </html> '
+				'<html> <head> <meta name="viewport" content="width=device-width,minimum-scale=1.0,initial-scale=1,user-scalable=yes"> <style> * { margin: 0; padding: 0; border: 0; box-sizing: border-box; } body { font: 14px/1.6 sans; color: #284900; background: #fff; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; min-width: 320px; max-width: 450px; } header { padding-left: 10px; color: white; background-color: #284900; display: flex; flex-wrap: nowrap; align-content: baseline; margin-bottom: 10px; } h1 { font-size: 1.2rem; flex: 1 1 auto; padding: 2px 0;} legend { margin-right: 1em; font-weight: bold; } button { color: #fff; background: transparent; min-width: 25px; border: dotted 2px transparent; font-size: inherit;  cursor: pointer; } button:hover,button:focus { background: #506b2f; } button::-moz-focus-inner { border: 0; } header button:focus { border-color: #fff; outline: 0; } button + button { margin-left: 3px; } main { padding: 0 10px 10px; } textarea { border: solid 1px #284900; width: 100%; height: 200px; padding: 5px; resize: none; } </style> </head> <body> <header> <h1>A11Y Report</h1> <button id="bmin"aria-label="minimize" data-action="minimize">&#128469;</button><button id="bmax" aria-label="maximize" data-action="maximize" style="display:none">&#128470;</button><button aria-label="close" data-action="close">&#215;</button> </header> <main id="result" class="result">  <textarea id="output" wrap="off" spellcheck="false"></textarea> </main> </body> </html> '
 				), doc.close();
 			var t = doc.querySelector('[data-action="close"]');
 			t && t.addEventListener("click", function(e) {
 				document.body.removeChild(container);
 			});
+			t = doc.getElementById('bmin');
+			t && t.addEventListener("click", function(e) {
+				doc.querySelector('main').style.display = 'none';
+				doc.getElementById('bmin').style.display = 'none';
+				doc.getElementById('bmax').style.display = 'block';
+				let h = iframe.contentWindow.document.body.offsetHeight + 'px';
+				iframe.style.height = h;
+				container.style.height = h;
+			});				
+			t = doc.getElementById('bmax');
+			t && t.addEventListener("click", function(e) {
+				doc.querySelector('main').style.display = 'block';
+				doc.getElementById('bmax').style.display = 'none';
+				doc.getElementById('bmin').style.display = 'block';
+				let h = iframe.contentWindow.document.body.offsetHeight + 'px';
+				iframe.style.height = h;
+				container.style.height = h;
+			});				
+						
 		}, document.body.appendChild(container);
 
 		return iframe;
