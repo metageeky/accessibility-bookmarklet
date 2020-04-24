a5397863ddc610f0b644262e4c4973b865cf3268
kate deibel <the.kate.deibel@gmail.com>
2020-04-24 18:55:46 -0400

    error

--- libraries/a11y-core-functions.js
+++ libraries/a11y-core-functions.js
@@ -171,7 +171,6 @@ function runA11YTool(callback) {
 				console.log('runTool else: ')
 				console.log(document.A11Y_REQUIREMENTS);
 				//runA11yTool(callback);
-				arguments.callee(callback);
 			}
 		}).catch(function(req) {
 			console.log(req);
